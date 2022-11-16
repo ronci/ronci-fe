3ad46410d57829c909039231efb2f49c79953828
2019015414 <2019015414@hanyang.ac.kr>
2022-11-17 01:08:31 +0900

    feat: implement Header UI when logged in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
+import { useContext, useEffect } from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
+import { UserNameContext } from '../provider/UserNameProvider';
+import { FlexBox } from './@shared/FlexBox';
+import { useGetUserInfo } from '../requestAPI';
 
 const Header = () => {
   return (
@@ -7,14 +11,46 @@ const Header = () => {
       <Link href='/'>
         <Title role='button'>HAUS</Title>
       </Link>
-      <Link href='/login'>
-        <button>login</button>
-      </Link>
+      <LoginInfo />
     </HeaderWrapper>
   );
 };
 
 export default Header;
+
+const LoginInfo = () => {
+  const { userName, setUserName } = useContext(UserNameContext);
+  const userId = typeof window !== 'undefined' ? localStorage.getItem('userId') : null;
+  const { refetch } = useGetUserInfo(
+    { userId: String(userId) },
+    {
+      enabled: !userId,
+      onSuccess: ({ data }) => {
+        const { NAME } = data.data.user;
+        setUserName(NAME);
+      },
+    }
+  );
+
+  useEffect(() => {
+    refetch();
+  }, [userId]);
+
+  if (userName) {
+    return (
+      <FlexBox flexDirection='column' alignItems='flex-end'>
+        <NameText>{userName}</NameText>
+        <button>logout</button>
+      </FlexBox>
+    );
+  }
+
+  return (
+    <Link href='/login'>
+      <button>login</button>
+    </Link>
+  );
+};
 
 const HeaderWrapper = styled.header`
   display: flex;
@@ -25,4 +61,8 @@ const HeaderWrapper = styled.header`
 
 const Title = styled.a`
   font-size: 48px;
+`;
+
+const NameText = styled.p`
+  font-size: 18px;
 `;

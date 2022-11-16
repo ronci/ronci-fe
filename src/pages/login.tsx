f8ab4943a477ff69e6667c201ad293679e65f156
2019015414 <2019015414@hanyang.ac.kr>
2022-11-16 23:46:27 +0900

    feat: add login submit event

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,20 +1,44 @@
 import type { NextPage } from 'next';
+import { FormEventHandler, useContext } from 'react';
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header';
 import Input from '../components/Input';
 import useInput from '../hooks/useInput';
+import { usePostLogin } from '../requestAPI';
 import { validateId, validateInputs, validatePassword } from '../utilities/validator';
+import { useRouter } from 'next/router';
+import { UserNameContext } from '../provider/UserNameProvider';
 
 const LoginPage: NextPage = () => {
+  const router = useRouter();
+  const { userName, setUserName } = useContext(UserNameContext);
   const loginId = useInput('', validateId);
   const loginPassword = useInput('', validatePassword);
+  const { mutate: mutatePostLogin } = usePostLogin({
+    onSuccess: ({ data }) => {
+      const {
+        accessToken,
+        user: { ID, NAME },
+      } = data.data;
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('userId', ID);
+      setUserName(NAME);
+      router.push('/');
+    },
+  });
+
   const isValidLogin = validateInputs([loginId, loginPassword]);
+
+  const login: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    mutatePostLogin({ id: loginId.value, password: loginPassword.value });
+  };
 
   return (
     <>
       <Header />
-      <Form>
+      <Form onSubmit={login}>
         <Input labelFor='loginId' labelName='아이디' inputType='text' {...loginId} />
         <Input
           labelFor='loginPassword'

3812d03d041050099e2aa868f48aaedd1d3b79bf
2019015414 <2019015414@hanyang.ac.kr>
2022-11-17 18:24:49 +0900

    feat: create infinite scroll page

--- src/pages/infinite-scroll.tsx
+++ src/pages/infinite-scroll.tsx
@@ -1,17 +1,36 @@
 import type { NextPage } from 'next';
-import React from 'react';
 import styled from 'styled-components';
 
-import products from '../api/data/products.json';
 import Header from '../components/Header';
 import ProductList from '../components/ProductList';
+import useIntersect from '../hooks/useIntersect';
+import { useGetProductListInfinite } from '../requestAPI';
 
 const InfiniteScrollPage: NextPage = () => {
+  const {
+    data: productListData,
+    hasNextPage: hasNextProductListPage,
+    fetchNextPage: fetchNextProductListPage,
+  } = useGetProductListInfinite();
+  const targetRef = useIntersect<HTMLDivElement>((entry, observer) => {
+    if (hasNextProductListPage) {
+      fetchNextProductListPage();
+    }
+    observer.unobserve(entry.target);
+  });
+
+  if (typeof productListData === 'undefined') {
+    return null;
+  }
+
+  const products = productListData.pages.flatMap((page) => page.data.data.products);
+
   return (
     <>
       <Header />
       <Container>
         <ProductList products={products} />
+        <div ref={targetRef} />
       </Container>
     </>
   );

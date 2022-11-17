180c089dd7ab34e7485560dd66c5e446ba3dcc41
2019015414 <2019015414@hanyang.ac.kr>
2022-11-17 15:45:43 +0900

    feat: add link to product detail page

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styled from 'styled-components';
 
 import { Product } from '../types/product';
@@ -6,20 +7,23 @@ type ProductItemProps = {
   product: Product;
 };
 
-const ProductItem = ({ product: { name, thumbnail, price } }: ProductItemProps) => (
-  <Container>
-    <Thumbnail src={thumbnail ? thumbnail : '/defaultThumbnail.jpg'} />
-    <Name>{name}</Name>
-    <Price>{price.toLocaleString()}원</Price>
-  </Container>
+const ProductItem = ({ product: { id, name, thumbnail, price } }: ProductItemProps) => (
+  <Link href={`/products/${id}`}>
+    <Container>
+      <Thumbnail src={thumbnail ? thumbnail : '/defaultThumbnail.jpg'} />
+      <Name>{name}</Name>
+      <Price>{price.toLocaleString()}원</Price>
+    </Container>
+  </Link>
 );
 
 export default ProductItem;
 
-const Container = styled.a`
+const Container = styled.div`
   width: 180px;
   margin-left: 20px;
   margin-top: 20px;
+  cursor: pointer;
 `;
 
 const Thumbnail = styled.img`

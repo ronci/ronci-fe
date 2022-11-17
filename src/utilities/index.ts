8654422bb08ad1d4920ec2f46fc854fc3f2c7845
2019015414 <2019015414@hanyang.ac.kr>
2022-11-17 11:54:54 +0900

    feat: implement findPageNumbers function

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -11,3 +11,23 @@ export const parseQueryString = (search: string): Record<string, string> =>
 
       return acc;
     }, {} as Record<string, string>);
+
+type FindPageNumbersFunction = (props: {
+  currentPage: number;
+  pageUnit: number;
+  totalPages: number;
+}) => {
+  startPage: number;
+  endPage: number;
+};
+
+export const findPageNumbers: FindPageNumbersFunction = ({ currentPage, pageUnit, totalPages }) => {
+  const isLastPageNumberInSet = currentPage % pageUnit ? 0 : 1;
+  const pageSetStartNumber =
+    Math.floor((currentPage - isLastPageNumberInSet) / pageUnit) * pageUnit;
+  const startPage = pageSetStartNumber + 1;
+  const endPage =
+    pageSetStartNumber + pageUnit > totalPages ? totalPages : pageSetStartNumber + pageUnit;
+
+  return { startPage, endPage };
+};

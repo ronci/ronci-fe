f3e5ac59a89fa8ed4267e6ef1087f8b419b6c386
2019015414 <2019015414@hanyang.ac.kr>
2022-11-17 21:21:55 +0900

    chore: add image domain

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-}
+  images: {
+    domains: ['contents.sixshop.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
